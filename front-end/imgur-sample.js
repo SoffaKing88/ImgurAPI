74607f0ea035e6e13c5808148c5c3d11ad492e18
Nick Soffa <nsoffa@lion.lmu.edu>
2015-11-04 23:03:50 -0800

    Copied code over from Private Repo

--- front-end/imgur-sample.js
+++ front-end/imgur-sample.js
@@ -1,8 +1,94 @@
 $(function () {
 
+	$.ajax({
+        url: "https://api.imgur.com/3/gallery/hot/viral/0.json",
+        headers: {
+            "Authorization": "Client-ID 60a9f235e6726aa"
+        }
+    }).done(function (result) {
+    	var j = 10
+    	for(var i = 0; i < j; i++){
+    		albumLink = "http://imgur.com/a/";
+    		console.log(result.data[i].link);
+    		if(!((result.data[i].link).startsWith(albumLink))){
+    			img = $('<img>').attr('src', result.data[i].link).addClass("thumbnail");
+    			$("#pop-images").append(img);
+    		}
+    		if((result.data[i].link).startsWith(albumLink)){
+    			j++
+    		}
+    	}
+    	$("#pop-images").ready(function() {
+    		$('img').each(function (){
+    			var currentImage = $(this);
+    			currentImage.wrap("<a href='" + currentImage.attr("src") + "' </a>");
+    		});
+    	});
+    });
+
+    $.ajax({
+        url: "https://api.imgur.com/3/g/memes/",
+        headers: {
+            "Authorization": "Client-ID 60a9f235e6726aa"
+        }
+    }).done(function (result) {
+    	var j = 10
+    	for(var i = 0; i < j; i++){
+    		albumLink = "http://imgur.com/a/";
+    		console.log(result.data[i].link);
+    		if(!((result.data[i].link).startsWith(albumLink))){
+    			image_url = result.data[i].link;
+    			img = $('<img>').attr('src', result.data[i].link).addClass("thumbnail");
+    			$("#meme-images").append(img);
+    		}
+    		if((result.data[i].link).startsWith(albumLink)){
+    			j++
+    		}
+    	}
+    	$("#meme-images").ready(function() {
+    		$('img').each(function (){
+    			var currentImage = $(this);
+    			currentImage.wrap("<a href='" + currentImage.attr("src") + "' </a>");
+    		});
+    	});
+    });
+
+    $("#subreddit-button").click(function () {
+		$.ajax({ 
+    		url: 'https://api.imgur.com/3/gallery/r/' + $("#subreddit-term").val(),
+    		headers: {
+        		'Authorization': 'Client-ID 60a9f235e6726aa'
+    		},
+    		data: {
+			'q' : $("#subreddit-term").val()
+			}
+		}).done(function (result) {
+			$("#subreddit-images").empty()
+			var j = 10
+			for(var i = 0; i < j; i++){
+				albumLink = "http://imgur.com/a/";
+				console.log(result.data[i].link);
+				if(!((result.data[i].link).startsWith(albumLink))){
+					img = $('<img>').attr('src', result.data[i].link).addClass("thumbnail");
+					$("#subreddit-images").append(img);
+				}
+				if((result.data[i].link).startsWith(albumLink)){
+					j++
+				}
+			}
+
+			$("#subreddit-images").ready(function() {
+    			$('img').each(function (){
+    				var currentImage = $(this);
+    				currentImage.wrap("<a href='" + currentImage.attr("src") + "' </a>");
+    			});
+    		});
+		});
+	});
+
     $("#search-button").click(function () {
 		$.ajax( {
-			url: "http://api.imgur.com/3/gallery/search/",
+			url: "https://api.imgur.com/3/gallery/search/",
 				headers: {
 					"Authorization": "Client-ID 60a9f235e6726aa"
 				},
@@ -11,46 +97,58 @@ $(function () {
 				}
 			}
 		).done(function (result) {
-			console.log(result)
-			//img = $('<img>').attr('src', result.data.link);
-			//console.log(img);
-			//$("#single-image").append(img);
-			//console.log(result.data.link);
-			//$('#search-button').attr("disabled", true);
+			$("#search-images").empty()
+			var j = 10
+			for(i = 0; i < j; i++){
+				albumLink = "http://imgur.com/a/";
+				console.log(result.data[i].link);
+				if(!((result.data[i].link).startsWith(albumLink))){
+					img = $('<img>').attr('src', result.data[i].link).addClass("thumbnail");
+					$("#search-images").append(img);
+				}
+				if((result.data[i].link).startsWith(albumLink)){
+					j++
+				}
+			}
+
+			$("#search-images").ready(function() {
+    			$('img').each(function (){
+    				var currentImage = $(this);
+    				currentImage.wrap("<a href='" + currentImage.attr("src") + "' </a>");
+    			});
+    		});
 		});
 	});
 
-	$("#upload-button").click(function () {
-		$.ajax({ 
-    			url: 'https://api.imgur.com/3/upload',
-    			headers: {
-        			'Authorization': 'Client-ID 60a9f235e6726aa'
-    			},
-    			type: 'POST',
-    			data: {
-        		'image': 'venom.jpg'
-    			}
+	$("#random-button").click(function () {
+		$.ajax( {
+			url: "https://api.imgur.com/3/gallery/random/",
+				headers: {
+					"Authorization": "Client-ID 60a9f235e6726aa"
+				},
+			}
+		).done(function (result) {
+			var j = 10
+			$("#random-images").empty();
+			for(var i = 0; i < j; i++){
+				albumLink = "http://imgur.com/a/";
+				console.log(result.data[i].link);
+				if(!((result.data[i].link).startsWith(albumLink))){
+					img = $('<img>').attr('src', result.data[i].link).addClass("thumbnail");
+					$("#random-images").append(img);
+				}
+				if((result.data[i].link).startsWith(albumLink)){
+					j++
+				}
+			}
+
+			$("#random-images").ready(function() {
+    			$('img').each(function (){
+    				var currentImage = $(this);
+    				currentImage.wrap("<a href='" + currentImage.attr("src") + "' </a>");
+    			});
+    		});
 		});
 	});
 
-	//$.ajax({
-	//	url: "https://api.imgur.com/3/account/me",
-	//	headers: {
-	//		"Authorization": "Client-ID 60a9f235e6726aa"
-	//	}
-	//}).done(function (result) {
-	//	console.log(result);
-	//});
-
-    $.ajax({
-        url: "https://api.imgur.com/3/gallery/hot/viral/0.json",
-        headers: {
-            "Authorization": "Client-ID 60a9f235e6726aa"
-        }
-    }).done(function (result) {
-    	//img = $('<img>').attr('src', result.data.link);
-    	//console.log(img);
-    	//$("#pop-images").append(img);
-        console.log(result);
-    });
 });
